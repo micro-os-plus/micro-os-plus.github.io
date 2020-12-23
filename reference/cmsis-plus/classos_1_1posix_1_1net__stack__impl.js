b1ee5c95db3b5d8dd15fcff759212b6156fed2de
Liviu Ionescu <ilg@livius.net>
2020-12-23 16:11:57 +0200

    remove top folders except docs

--- reference/cmsis-plus/classos_1_1posix_1_1net__stack__impl.js
+++
@@ -1,7 +0,0 @@
-var classos_1_1posix_1_1net__stack__impl =
-[
-    [ "net_stack_impl", "classos_1_1posix_1_1net__stack__impl.html#ae123b59232bbf2101e27b155ec01d88c", null ],
-    [ "~net_stack_impl", "classos_1_1posix_1_1net__stack__impl.html#adde4515607e975d9f7317d8dd77887e4", null ],
-    [ "do_socket", "classos_1_1posix_1_1net__stack__impl.html#adc0fb2e288cdbe3d8f171df48529ad06", null ],
-    [ "interface", "classos_1_1posix_1_1net__stack__impl.html#afc0a5685219031503689c691b4bc7619", null ]
-];
--- reference/cmsis-plus/search/typedefs_10.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['unique_5fptr',['unique_ptr',['../group__cmsis-plus-rtos-memres.html#gaec618354c35d9d8269647786d0c6bd6a',1,'os::rtos::memory']]]
-];
